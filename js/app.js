2e8a91515e2954dde32a2bdf631364873ece514b
HussnainAhmad1606 <hussnainahmad1606@gmail.com>
2021-03-01 16:24:04 +0500

    Add icons to UI remove buttons, add spin and reverse spin motion on cover pic, play next track after completing the previous track, clean and add comments to the code.

--- js/app.js
+++ js/app.js
@@ -1,51 +1,80 @@
-console.log("Music Player");
+/*
+Application Name: Music Player
+Version: 1.0
+Developer: HussnainAhmad160
+
+*/
+
+
+// Array of all musics
 var tracks = [
 {
+	src: "audio/bulleya.mp3",
+	artist: "Unknown Artist",
+	track: "Bulleya"
+
+	},
+	{
 	src: "audio/alan.mp3",
 	artist: "Alan Walker",
-	track: "Faded Official Audio"
+	track: "Faded"
 
-},
-{
- src: "audio/move.mp3",
- artist: "Armaan Malik",
-track: "Ready to Move"
+	},
+	{
+ 	src: "audio/move.mp3",
+ 	artist: "Armaan Malik",
+	track: "Ready to Move"
 
-},
-{
-src: "audio/samjh.mp3",
-artist: "Arijit Singh",
-	track: "Main tenu Samjhavan Ki"
+	},
+	{
+	src: "audio/samjh.mp3",
+	artist: "Arijit Singh",
+	track: "Main tenu Samjhavaan Ki"
 }];
 
-var i = 0;
-function playAudio() {
 
+var i = 0;
+
+// Function for playing audio
+const playAudio = () => {
+
+	// Spining motion of music icon
 	let cover = document.getElementById("cover");
-	cover.setAttribute("style", "animation: rotate 5s infinite ease;")
+	cover.setAttribute("style", "animation: rotate 10s infinite linear;");
+
+	// Playing the audio
 	let audio = document.getElementById("audio");
 	audio.play();
+
+	// Changing play button to pause button
 	let button = document.getElementById("action");
-	button.innerHTML = `<button id="play" class="btn" onclick="pauseAudio();">Pause</button>`;
+	button.innerHTML = `<img class="control-button" onclick="pauseAudio();" id="pauseBtn" src="img/pause.png" alt="">`;
 
 
 }
 
-function pauseAudio() {
+const  pauseAudio = () => {
+	// Stoping the spining motion of the music icon
 	let cover = document.getElementById("cover");
-	cover.setAttribute("style", "animation: none;")
+	cover.setAttribute("style", "animation: none;");
+
+	// Stopping the audio
 	let audio = document.getElementById("audio");
 	audio.pause();
+
+	// Changing the pause button to play button
 	let button = document.getElementById("action");
-	button.innerHTML = `<button id="play" class="btn" onclick="playAudio();">Play</button>`;
+	button.innerHTML = `<img class="control-button" onclick="playAudio();" id="playBtn" src="img/play.png" alt="">`;
 }
 
+
+// Function for playing next track..
 const nextTrack = () => {
-	if (i == 2) {
-		i = 2;
+	if (i == 3) {
+		i = 3;
 	}
 	else {
-		i += 1	
+		i += 1;
 	}
 	document.getElementById("name").textContent = tracks[i].track;
 	document.getElementById("artist").textContent = tracks[i].artist;
@@ -54,15 +83,23 @@ const nextTrack = () => {
 }
 
 
+// Function for playing previous track...
 const previousTrack = () => {
 	if (i == 0) {
 		i = 0;
 	}
 	else {
-		i -= 1		
+		i -= 1;
 	}
 	document.getElementById("name").textContent = tracks[i].track;
 	document.getElementById("artist").textContent = tracks[i].artist;
 	audio.src = tracks[i].src;
 	playAudio();
 }
+
+
+// If audio is finished, play the next audio
+document.getElementById("audio").addEventListener("ended", function() {
+	console.log("Playing Next Song...");
+	nextTrack();
+});
